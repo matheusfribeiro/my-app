ff98a6bc9a42d277c37a0c2fdd422f6ec3a29ecb
Matheus F. Ribeiro <matheusfribeiro@outlook.com>
2021-11-24 14:35:50 -0300

    uploading initial structure

---
+++ src/partials/Header/Header.style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1,
+        marginLeft: 10,
+    }
+
+}))
+
+export default useStyles
